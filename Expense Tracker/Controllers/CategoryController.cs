e9eb7cfcc7f8b071ed0cc9ea11da0f1cd9db24c4
YousofAhmad0 <yousofahmad81@gmail.com>
2022-09-08 15:41:09 +0300

    Done with delete but conform delete needs chech!

--- Expense Tracker/Controllers/CategoryController.cs
+++ Expense Tracker/Controllers/CategoryController.cs
@@ -27,24 +27,6 @@ namespace Expense_Tracker.Controllers
                           Problem("Entity set 'AppDbContext.Categories'  is null.");
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // GET: Category/Create
         public IActionResult AddOrEdit(int id = 0)
         {
@@ -76,25 +58,6 @@ namespace Expense_Tracker.Controllers
             return View(category);
         }
 
-        
-
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
 
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -115,9 +78,5 @@ namespace Expense_Tracker.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryExists(int id)
-        {
-          return (_context.Categories?.Any(e => e.CategoryId == id)).GetValueOrDefault();
-        }
     }
 }
