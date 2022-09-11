9a26a1d2118ec79b69b655d25a78840b58905295
yousofahmad0 <yousofahmad81@gmail.com>
2022-09-11 16:28:36 +0300

    Adding IncomeTotal, ExpenseTotal and Balance to Dashboard

--- Expense Tracker/Controllers/DashboardController.cs
+++ Expense Tracker/Controllers/DashboardController.cs
@@ -1,11 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Expense_Tracker.Data;
+using Expense_Tracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 namespace Expense_Tracker.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        public DashboardController(AppDbContext context)
         {
+            _context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+            //Last 7 days
+            DateTime startDate = DateTime.Today.AddDays(-6);
+            DateTime endDate = DateTime.Today;
+
+            List<Transaction> SelectedTransactions = await _context.Transactions
+                .Include(x => x.Category)
+                .Where(y => y.Date >= startDate && y.Date<=endDate)
+                .ToListAsync();
+
+            //Total Income
+            int TotalIncome = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .Sum(j => j.Amount);
+
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
+
+            //Total Expense
+            int TotalExpense = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .Sum(j => j.Amount);
+
+            ViewBag.TotalExpense = TotalExpense.ToString("C0");
+
+            //Balance
+            int Balance = TotalIncome - TotalExpense;
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+            culture.NumberFormat.CurrencyNegativePattern = 1;
+            ViewBag.Balance = String.Format(culture,"{0:C0}",Balance);
+
             return View();
         }
     }
